9f2be97076cecb2f64241d3f1ec4f3399ab1cce6
fdemarco720 <fdemarco720@alumno.unlam.edu.ar>
2024-06-19 20:42:13 -0300

    detalles

--- js/detalle-pelicula.js
+++ js/detalle-pelicula.js
@@ -4,74 +4,80 @@ const ARRAY_SERIES_ = JSON.parse(localStorage.getItem("datosSeriesStorage"));
 const URL_SELECCIONADO = document.location.href; //Consigo la url de la vista
 const URL_PASADO_A_STRING = new URL(URL_SELECCIONADO); //Paso la url a formato String mediante una clase propia de visualStudio
 const NOMBRE_SELECCIONADO = URL_PASADO_A_STRING.searchParams.get('nombre'); //Le indico que me devuelva el contenido que hay despues de la clave "nombre"
+const TIPO_SELECCIONADO = URL_PASADO_A_STRING.searchParams.get('tipo');
   
-    let tituloPelicula = document.querySelector("#titulo")
-    let nodo_texto = document.createElement("h3");
-    nodo_texto.textContent = `Titulo: ${NOMBRE_SELECCIONADO}`;
-    tituloPelicula.appendChild(nodo_texto);
+  
+    
+function agregarTemporadas(){
+    let temporadas = document.querySelector("#temporadas");
+    let capitulos = document.querySelector("capitulos")
+    let nodo_temporadas = document.createElement("select"); //meter el select dentro de temporadas
+    let i=1;
+   
 
-    const ARRAY_PELICULAS_Y_SERIES = [];
-    
-    for (let pelicula of ARRAY_PELICULAS_) {
-    ARRAY_PELICULAS_Y_SERIES.push(pelicula);
-       }
-    
     for (let series of ARRAY_SERIES_) {
-        ARRAY_PELICULAS_Y_SERIES.push(series);
-           }
-  
-    function buscarDatosParaDetalles(){  
-        
-        let video = document.querySelector("#iframePelicula");  
-        let genero = document.querySelector("#genero");
-        let duracion = document.querySelector("#duracion");
-        let sinopsis = document.querySelector("#sinopsis")
-        let nodo_genero = document.createElement("h3");
-        let nodo_duracion = document.createElement("h3");
-        let nodo_sinopsis = document.createElement("h3");
-        let nodo_btn = document.querySelector("#btn_comenzar");
-        let nodo_select = document.createElement("select")
-        
+        if(series.nomr)
+        let nodo_option = document.createElement("option");
+        nodo_option.value = i;
+        nodo_option.textContent = temp[temp];
+        nodo_temporadas.appendChild(nodo_option);
+        i++;
+    }
+    temporadas.appendChild(nodo_temporadas);
+}
 
-        for (let pelicula_serie of ARRAY_PELICULAS_Y_SERIES) {
-            if(NOMBRE_SELECCIONADO == pelicula_serie.nombrePelicula){
-                if(ARRAY_PELICULAS_.includes(pelicula_serie)){
-                nodo_genero.textContent = `Categoría: ${pelicula_serie.genero}`;
-                nodo_duracion.textContent = `Duración: ${pelicula_serie.duracion} minutos`;
-                nodo_sinopsis.textContent = `Sinopsis: ${pelicula_serie.sinopsis}`;
-                genero.appendChild(nodo_genero);
-                duracion.appendChild(nodo_duracion);
-                sinopsis.appendChild(nodo_sinopsis);
-                video.src = pelicula_serie.iframe;
-                nodo_btn.href = pelicula_serie.linkVideo;
-                nodo_btn.target = "_blank";
-                }else{
-                    nodo_genero.textContent = `Categoría: ${pelicula_serie.genero}`;
-                    // nodo_duracion.textContent = `Temporada: ${pelicula_serie.duracion} minutos`;
-                    nodo_sinopsis.textContent = `Sinopsis: ${pelicula_serie.sinopsis}`;
-                    genero.appendChild(nodo_genero);
-                    duracion.appendChild(nodo_duracion);
-                    sinopsis.appendChild(nodo_sinopsis);
-                    video.src = pelicula_serie.iframe;
-                    nodo_btn.href = pelicula_serie.linkVideo;
-                    nodo_btn.target = "_blank";
-                }
-                
+    function buscarDatosParaDetallesPeliculas(){  
+        for (let pelicula of ARRAY_PELICULAS_) {
+            if(NOMBRE_SELECCIONADO == pelicula.nombrePelicula){
+            nodo_genero.textContent = `Categoría: ${pelicula.genero}`;
+            nodo_duracion.textContent = `Duración: ${pelicula.duracion} minutos`;
+            nodo_sinopsis.textContent = `Sinopsis: ${pelicula.sinopsis}`;
+            genero.appendChild(nodo_genero);
+            duracion.appendChild(nodo_duracion);
+            sinopsis.appendChild(nodo_sinopsis);
+            video.src = pelicula.iframe;
+            nodo_btn.href = pelicula.linkVideo;
+            nodo_btn.target = "_blank";
             }
         }
     }
-    buscarDatosParaDetalles();
+
+    let titulo = document.querySelector("#titulo")
+    let nodo_texto = document.createElement("h3");
+    nodo_texto.textContent = `Titulo: ${NOMBRE_SELECCIONADO}`;
+    titulo.appendChild(nodo_texto);
+      
+    let video = document.querySelector("#iframePelicula");  
+    let genero = document.querySelector("#genero");
+    let duracion = document.querySelector("#duracion");
+    let sinopsis = document.querySelector("#sinopsis")
+    let nodo_btn = document.querySelector("#btn_comenzar");
+    let nodo_genero = document.createElement("h3");
+    let nodo_duracion = document.createElement("h3");
+    let nodo_sinopsis = document.createElement("h3");
+
+    if(TIPO_SELECCIONADO == "pelicula"){
+        buscarDatosParaDetallesPeliculas();
+        }
 
 
+        function buscarDatosParaDetallesSeries(){  
+            for (let serie of ARRAY_SERIES_) {
+                if(NOMBRE_SELECCIONADO == serie.nombreSerie){
+                nodo_genero.textContent = `Categoría: ${serie.genero}`;
+                nodo_sinopsis.textContent = `Sinopsis: ${serie.sinopsis}`;
+                genero.appendChild(nodo_genero);
+                agregarTemporadas();
+                sinopsis.appendChild(nodo_sinopsis);
+                video.src = serie.iframe;
+                nodo_btn.href = serie.linkVideo;
+                nodo_btn.target = "_blank";
+                }
+            }
+        }
 
-    //AGREGAR CATEGORIAS AL SELECT
-    let generosAgregados = []; // Array para almacenar los géneros que ya han sido agregados
+    if(TIPO_SELECCIONADO=="serie"){
+        buscarDatosParaDetallesSeries();
+    }
 
-
-    for (let cantidad in pelicula_serie.temporada) {
-        let nodo_option = document.createElement("option");
-        nodo_option.value = cantidad.length+1;
-        nodo_option.textContent = `${cantidad.length+1}`;
-
-        duracion.appendChild(nodo_option);
-}
+    
