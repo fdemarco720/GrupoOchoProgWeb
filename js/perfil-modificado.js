5a7beafb1ed40f901ff598589db65bc7dbc910ba
MartinMeza22 <martinmeza.t22@gmail.com>
2024-06-21 20:00:28 -0300

    ejje

--- js/perfil-modificado.js
+++ js/perfil-modificado.js
@@ -1,14 +1,26 @@
 //Regex
-const regexSolonNumerosYLetras = /[A-Za-z0-9]/;
-const regexSoloContraseniasValidas = /^.*[A-Za-z].*[A-Za-z].*[0-9].*[0-9].*[!@#\$%\^&\*()_\+\-=\[\]{};':"\\|,.<>\/?].*[!@#\$%\^&\*()_\+\-=\[\]{};':"\\|,.<>\/?]$/;
-//
+const regexSolonNumerosYLetras = /^[A-Za-z0-9]$/;
+//.*[A-Za-z] $ / * + 
+const regexSoloContraseniasValidas =/^(?=(.*[A-Za-z]){2})(?=(.*[0-9]){2})(?=(.*[!@#\$%\^&\*()_\+\-=\[\]]){2}).{8,}$/;
+
+const usuarios = JSON.parse(localStorage.getItem("usuarios"));
+const usuarioActual = JSON.parse(localStorage.getItem('usuarioActual')) || [];
+const usuarioEncontrado = buscarUsuario(usuarioActual);
 
 const campoNombreUsuario = document.querySelector('#user');
-const campoMail = document.querySelector('#em');
+const campoMail = document.querySelector('#mailDinamico');
+
+campoNombreUsuario.textContent = usuarioEncontrado.nombreUsuario;
+camp
+
+console.log(usuarioEncontrado);
+
 const campoContraseniaActual = document.querySelector('#contrasenia');
 //agrego un <P> que se modifique cada vez que el usuario cambie su contraseña  
 let campoContraseniaActualDinamico = document.createElement('p');
-campoContraseniaActualDinamico.textContent = "Dar dinamismo";
+//campoContraseniaActualDinamico = usuarios.JSON.parse('usuarioActual');
+//let contraseniaaa = campoContraseniaActualDinamico.nombre;
+//campoContraseniaActualDinamico.textContent = contraseñaDinamica();
 campoContraseniaActual.appendChild(campoContraseniaActualDinamico);
 //
 const campoNuevaContrasenia = document.querySelector('#NC');
@@ -27,15 +39,12 @@ const botonCancelarSubscripcion = document.querySelector('#cancelar-subscripcion
 let errores = "";
 let errorFinal = "";
 
-const usuarios = JSON.parse(localStorage.getItem("usuarios"));
-const usuarioActual = + JSON.parse(localStorage.getItem('usuarioActual')) || [];
-
 console.log(usuarios)
 
 function buscarUsuario(usuarioActual){
     let usuarioEncontrado = null;
     for(let usuario of usuarios){
-        if(usuario.nombre == usuarioActual.nombre){
+        if(usuario.nombre === usuarioActual.nombre){
             usuarioEncontrado = usuario;
         }
     }
@@ -50,33 +59,28 @@ function validacionDeFormulario(evento){
         sePudoValidar = regexSoloContraseniasValidas.test(variableAValidar);
         return sePudoValidar;
     }
-
+    //F0
     function validacionDeContraseniasIguales(){ //Este metodo solo se debe llamar en caso de que el usuario quiera guardar cambios y los campos de contraseñas esten completos
         let sonIguales = false;
-        console.log("CA" + campoContraseniaActual);
-        console.log("NC" + campoNuevaContrasenia);
-        console.log("RC" + campoRepetirContrasenia);
-        
-        if(campoNuevaContrasenia && campoRepetirContrasenia){
-            if(validacionDeContrasenia(campoNuevaContrasenia) && campoNuevaContrasenia == campoRepetirContrasenia){
+            if(validacionDeContrasenia(campoNuevaContrasenia.value) && campoNuevaContrasenia.value == campoRepetirContrasenia.value){
                 sonIguales = true;
+                campoContraseniaActual.value = campoNuevaContrasenia.value; //Actualizo contraseña
             }else{
                 errores += "- Las contraseñas no coinciden\n";
             }
-        }
         return sonIguales;
     }
-
+    //F1
     function elCodigoCVVEsCorrecto(){ //Este metodo solo debe ser llamado en caso de que el usuario quiera guardar cambios y seleccione el radio button de tarjeta de credito
         let esCorrecto = false;
-        if(ingresoCodigoCVV != '000' && ingresoCodigoCVV.value.length == 3){
+        if(ingresoCodigoCVV.value != '000' && ingresoCodigoCVV.value.length == 3){
             esCorrecto = true;
         }else{
             errores += "- El codigo CVV debe contener 3 digitos y debe ser distinto de '000'\n";
         }
         return esCorrecto;
     }
-
+    //F1
     function elLargoDeLaTarjetaDeCreditoEsCorrecto(){ //Este metodo solo debe ser llamado en caso de que el usuario quiera guardar cambios y seleccione el radio button de tarjeta de credito
         let esCorrecto = false;
         if(ingresoNumeroTarjeta.value.length >= 16 && ingresoNumeroTarjeta.value.length <= 19){
@@ -86,34 +90,39 @@ function validacionDeFormulario(evento){
         }
         return esCorrecto;
     }
+    //F1
+    function esCorrectoElIngresoDeTarjetaDeCredito(){ //Pregunta si se quiere y si se puede cambiar el metodo de pago
+        let esCorrecto = false;
+        if(radioButtonTarjetaDeCredito.checked && elCodigoCVVEsCorrecto() && elLargoDeLaTarjetaDeCreditoEsCorrecto()){ //Condiciones para tarjeta de credito
+                esCorrecto = true;
+        }
+        return esCorrecto;
+    }
 
+    //F2
     function seDeseaCambiarElMetodoDePago(){ //Metodo que se debe usar si o si
         let seDeseaCambiar = false;
         if(radioButtonPagoFacil.checked || radioButtonRapiPago.checked || radioButtonTransferenciaBancaria.checked){
-            seDeseaCambiar = true;
+            seDeseaCambiar = true; //Da true si hay algún radio button seleccionado (que no sea tarjeta de credito)
         }
         return seDeseaCambiar;
     }
 
-    function sePuedeCambiarElMetodoDePago(){ //Pregunta si se quiere y si se puede cambiar el metodo de pago
-        let sePuede = false;
-        if((radioButtonTarjetaDeCredito.checked && elCodigoCVVEsCorrecto() && elCodigoCVVEsCorrecto()) //Condiciones para tarjeta de credito
-            || seDeseaCambiarElMetodoDePago){ // Condicion para los que no son tarjeta de credito
-                sePuede = true;
-        }
-        return sePuede;
-    }
-    console.log(validacionDeContraseniasIguales());
-    console.log(elCodigoCVVEsCorrecto());
-    console.log(elLargoDeLaTarjetaDeCreditoEsCorrecto());
-    console.log(seDeseaCambiarElMetodoDePago());
-    console.log(sePuedeCambiarElMetodoDePago());
-    console.log("\n");
 
+    //F3
+    function esValidoElCambioDeMetodoDePago(){
+        let esValido = false;
+        if((ingresoNumeroTarjeta.value.length == 0 && ingresoCodigoCVV.value.length == 0 && seDeseaCambiarElMetodoDePago() || (esCorrectoElIngresoDeTarjetaDeCredito() && !seDeseaCambiarElMetodoDePago()))){ 
+            esValido = true;    //Solo devuelve true si numero de tarjeta y codigo cvv estan vacios y esté seleccionado otro radio buton que no sea tarjeta de credito
+        }
+        return esValido;
+    }
+    
     function sePuedeGuardarLosCambios(){
         let sePuede = false;
-        if((validacionDeContraseniasIguales() && sePuedeCambiarElMetodoDePago()) || ((validacionDeContraseniasIguales() || sePuedeCambiarElMetodoDePago()) && errores == "")){ //Se analiza que cambios quiere realizar el usuario
+        if( ((validacionDeContraseniasIguales() && esValidoElCambioDeMetodoDePago() ) || (validacionDeContraseniasIguales() || esValidoElCambioDeMetodoDePago()) ) && errores == ""  ){ //Se analiza que cambios quiere realizar el usuario
             sePuede = true;
+            alert("Datos guardados exitosamente!")
             evento.target.submit();
         }else{
            errorFinal = "No se puede realizar los cambios requeridos. Datos erroneos";
@@ -125,3 +134,33 @@ function validacionDeFormulario(evento){
 };
 
 botonGuardarCambios.addEventListener('click', validacionDeFormulario);
+/*
+function contraseñaDinamica(){
+    let contenido = "";
+    for(let i = 0; i<campoContraseniaActual.value.length; i++){
+        if(contraseniaaa.charAt(i) != ""){
+            contenido += "*";
+        }
+    }
+    return contenido;
+}
+*/
+/**
+ * contraseñas validas  --------
+ * contraseñas iguales F0  ----------
+ * checked tarjeta de cedito && cvv && largo de tarjeta F1
+ * checked rapiPago || Pago Facil || Transferencia bancaria F2
+ * funcion 1 || funcion 2 validan si es valido F3
+ * F0 || F3 Se puede guardar cambios
+ * 
+ */
+
+
+/*
+console.log( "contraseñas iguales" + validacionDeContraseniasIguales());
+console.log("codigo cvv" + elCodigoCVVEsCorrecto());
+console.log("largo de tarjeta" + elLargoDeLaTarjetaDeCreditoEsCorrecto());
+console.log("Se quiere cambiar el metodo de pago" + seDeseaCambiarElMetodoDePago());
+console.log("Se puede" + sePuedeCambiarElMetodoDePago());
+console.log("\n");
+*/
